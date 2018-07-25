1772be8f57019e3946447351ac807d4bd6053dea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-25 16:06:38 +0200

    Deploy navikt/nav-frontend-moduler to github.com/navikt/nav-frontend-moduler.git:gh-pages

--- development/app/bootstrap.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './../../node_modules/nav-frontend-core/less/core.less';
-import App from './components/App';
-
-render(<App />, document.getElementById('development'));

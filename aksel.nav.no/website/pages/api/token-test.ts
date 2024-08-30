52515969f457b8b8e48f9264c92076a227019126
Ken <ken.aleksander@gmail.com>
2024-08-30 15:08:37 +0200

    TOKEN TESTING FIXED

--- aksel.nav.no/website/pages/api/token-test.ts
+++ aksel.nav.no/website/pages/api/token-test.ts
@@ -1,12 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function test(req: NextApiRequest, res: NextApiResponse) {
-  if (process.env.SANITY_PREVIEW_TOKEN) {
-    return res.status(200).json({ message: "Found token preview" });
-  }
-
   /* This keys is only for testing and will not compromise anything if leaked! */
   if (process.env.sanity_demo_key) {
+    if (process.env.SANITY_PREVIEW_TOKEN) {
+      return res.status(200).json({
+        message: `Found token demo-key : ${process.env.sanity_demo_key} + sanity tokens`,
+      });
+    }
     return res.status(200).json({
       message: `Found token demo-key : ${process.env.sanity_demo_key}`,
     });
